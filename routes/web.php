c8198fa6fb872aa51e267d6288d3af132386bbb2
hannnnnnnnn1414 <haniffathurrahmanrustanto@gmail.com>
2025-12-24 10:23:49 +0700

    Add revision detail view by account

--- routes/web.php
+++ routes/web.php
@@ -212,6 +212,9 @@ Route::middleware(['auth'])->group(function () {
     // Route::get('/budget-revision/detail/{revisionCode}', [BudgetRevisionController::class, 'detail'])
     //     ->name('budget-revision.detail');
 
+    Route::get('budget-revision/detail/{acc_id}', [BudgetRevisionController::class, 'detail'])
+        ->name('budget-revision.detail')->middleware('auth');
+
     Route::get('/budget-revision/department/{deptCode}', [BudgetRevisionController::class, 'detailByDepartment'])
         ->name('budget-revision.detail-by-department');
 
